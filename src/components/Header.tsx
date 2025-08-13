802e15e1b833d8d46e9343e8447a50f31a123f06
vars-002 <varshavarshan2002@gmail.com>
2025-08-13 04:59:18 -0700

    Fix logo import path

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import image from "../assets/profileImg.png";
-import Logo from "../assets/logo.png";
+import Logo from "../assets/Logo.png";
 
 const Header: React.FC = () => {
   return (
